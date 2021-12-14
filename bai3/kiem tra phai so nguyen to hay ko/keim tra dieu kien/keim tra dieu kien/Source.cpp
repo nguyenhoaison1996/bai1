f1b95524b652bd48f56d46cb4f9d2d6a0cb9f648
nguyenhoaison1996 <nguyenhoaisoncv5@gmail.com>
2021-12-14 21:50:00 +0700

    ok

---
+++ bai3/kiem tra phai so nguyen to hay ko/keim tra dieu kien/keim tra dieu kien/Source.cpp
@@ -0,0 +1,31 @@
+﻿#include<stdio.h>
+void main()
+{
+	int n = 0;
+	int dem = 0;
+	do
+	{
+		printf("\r\nnhap gia tri cua n =");
+		scanf_s("%d", &n);
+		printf("\r\nn = %d", n);
+	} while (n <= 0);
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			if (n%i==0)
+			{
+				dem++;
+			}
+		}
+		
+	}
+	if (dem ==2)
+	{
+		printf("la so nguyen to khong sai\r\n");
+
+	}
+	else
+	{
+		printf("khong phai so nguyen to\r\n");
+	}
+}
